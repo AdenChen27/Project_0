2f1709875574c14314b6f316a8ff2eba6fbcb25d
AdenChen27 <AdenChen2005@gmail.com>
2022-03-31 09:17:23 +0800

    load i18n in show-word-info page

--- project_0/settings.py
+++ project_0/settings.py
@@ -149,7 +149,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static/test')
+    os.path.join(BASE_DIR,'test/static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
