1b2e23d61027e1510d28e1717d3ee4bcd11bd8b5
AdenChen27 <AdenChen2005@gmail.com>
2022-03-31 09:12:56 +0800

    Removing ignored files

--- test/static/javascript/bootstrap-tooltip.js
+++
@@ -1,6 +0,0 @@
-// enable bootstrap tooltip
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
-
--- test/static/javascript/language-setting.js
+++
@@ -1,9 +0,0 @@
-function set_lang(lang) {
-    str="/i18n/setlang/";
-    myform = document.getElementById('testform');
-    myform.value = lang;
-    myform.method = "POST";
-    myform.action = str;
-    document.getElementById("language").value = lang;
-    myform.submit();
-}
